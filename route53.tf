9ced622462911ca2175ee811859b4ab0aeacc948
Raghu K <raghuk.vit@gmail.com>
2022-06-16 07:58:30 +0530

    Update roboshop-infra/all-modules/tf-module-rds-mysql/

--- route53.tf
+++ route53.tf
@@ -1,8 +1,8 @@
 resource "aws_route53_record" "elastic" {
   zone_id = var.PRIVATE_HOSTED_ZONE_ID
-  name    = "${var.COMPONENT}-redis-${var.ENV}.roboshop.internal"
+  name    = "mysql-${var.ENV}.roboshop.internal"
   type    = "CNAME"
   ttl     = "300"
-  records = [aws_elasticache_cluster.escluster.cache_nodes[0].address]
+  records = [aws_db_instance.main.address]
 }
 
